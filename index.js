9046650678cda57fdf0740d09bde48689ecb49f4
danen <endra1632@gmail.com>
2022-01-12 11:19:48 +0700

    fitst

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 8000;
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
+
+app.listen(port, () => {
+  console.log(`server berjalan di ${port}`);
+});
