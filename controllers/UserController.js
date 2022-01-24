4d753e752f8767069b5b5c88b7104c14b03c42ef
C4ptainN3mo <endra1632@gmail.com>
2022-01-24 14:42:38 +0700

    CRUD

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -44,4 +44,89 @@ const detail = async (req, res) => {
   }
 };
 
-module.exports = { index, detail };
+const detailByEmail = async (req, res) => {
+  try {
+    const { email } = req.params;
+    const users = await UserModel.findOne({
+      where: {
+        email: email,
+      },
+    });
+    if (users === null) {
+      return res.status(200).json({
+        status: "fail",
+        msg: "there's no email like this, LMAO",
+      });
+    }
+    return res.json({
+      status: "succes",
+      msg: "find it",
+      data: users,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(403).json({
+      status: "fail",
+      msg: "there's a mistake",
+    });
+  }
+};
+
+const destroy = async (req, res) => {
+  const { id } = req.params;
+
+  const users = await UserModel.destroy({
+    where: {
+      id: id,
+    },
+  });
+
+  if (users === 0) {
+    return res.status(403).json({
+      status: "fail",
+      msg: "there's no id like this",
+    });
+  }
+  return res.json({
+    status: "succes",
+    msg: "destroy it",
+  });
+};
+
+const update = async (req, res) => {
+  try {
+
+    const { id } = req.params;
+    const { name } = req.body;
+    const usersUpdate = await UserModel.findByPk(id);
+    if (usersUpdate === null) {
+      return res.json({
+        status: "fail",
+        msg: "there's no id like this, LMAO",
+      });
+    }
+    await UserModel.update(
+      {
+        name: name,
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+    return res.json({
+      status: "Berhasil",
+      messege: "User Berhasil Diupdate",
+    });
+  } catch (error) {
+    console.log(error);
+    console.log(error);
+    return res.status(403).json({
+      status: "fail",
+      msg: "there's a mistake",
+    });
+  }
+};
+
+module.exports = { index, detail, detailByEmail, destroy, update };
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const validationRegister = require("../validator/authValidator");
 const validationMiddleWare = require("../middleware/validationMiddleWare");
 const { register } = require("../controllers/AuthController");
-const { index, detail } = require("../controllers/UserController");
+const {
+  index,
+  detail,
+  detailByEmail,
+  destroy,
+  update
+} = require("../controllers/UserController");
 router.get("/", (req, res) => {
   res.send({ status: "ok" });
 });
@@ -14,4 +20,8 @@ router.post("/register", validationRegister, validationMiddleWare, register);
 
 router.get("/users", index);
 router.get("/users/:id", detail);
+router.get("/users/email/:email", detailByEmail);
+router.delete("/users/:id", destroy);
+router.put("/users/update/:id", update);
+
 module.exports = router;
