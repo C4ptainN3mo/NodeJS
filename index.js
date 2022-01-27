4dfcfb0f8d549884aa9d4500b20bebfc84868e71
C4ptainN3mo <endra1632@gmail.com>
2022-01-27 08:15:16 +0700

    cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 const router = require("./routes/index");
 const port = process.env.PORT || 8000;
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
